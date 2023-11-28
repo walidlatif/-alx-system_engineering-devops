f59bb0865a8b1bfdd4514bcf563f3935531f8485
walidlatif <walidlatif45@gmail.com>
2023-11-28 02:16:31 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,14 @@
 # install Flask v2.1.0
-package { 'python3-pip':
+package { 'python3':
   ensure => installed,
 }
 
-package { 'Flask':
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
